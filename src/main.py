b4e78970878d1c66445214bf3120822158fbdb07
Dhruv Kinger <cse1785@outlook.com>
2020-05-10 01:29:44 +0530

    update

--- src/main.py
+++ src/main.py
@@ -103,13 +103,55 @@ def main():
             break
 
 
-        count++
+        count+=1
 
         if count%5==0:
             cv2.imshow('video',cv2.resize(frame,(500,500)))
     
         key = cv2.waitKey(60)
-        
+        croppedFace, face_coords = Fd.predict(frame.copy(), args.prob_threshold)
+
+        if type(croppedFace)==int:
+            logger.error("Unable to detect the face.")
+            if key==27:
+                break
+            continue
+
+
+         hp_out=Hp.predict(croppedFace.copy())
+         
+         l_eye,r_eye,eye_coords=Fl.predict(croppedFace.copy())    # Main funcn's doing all our task
+
+         new_coord,gaze_vector=Ge.predict(l_eye,r_eye,hp_out)
+
+
+         ## Now comes the importance of all the flags
+
+         if (not len(Flags)==0):
+            new_frame = frame.copy()
+            if 'fd' in Flags:
+                new_frame = croppedFace
+
+            if 'fl' in Flags:
+                cv2.rectangle(croppedFace, (eye_coords[0][0]-5, eye_coords[0][1]-5), (eye_coords[0][2]+5, eye_coords[0][3]+5), (0,255,0), 3)
+                cv2.rectangle(croppedFace, (eye_coords[1][0]-5, eye_coords[1][1]-5), (eye_coords[1][2]+5, eye_coords[1][3]+5), (0,255,0), 3)
+                
+
+            if 'hp' in Flags:
+                cv2.putText(new_frame, "Pose Angles: yaw:{:.2f} | pitch:{:.2f} | roll:{:.2f}".format(hp_out[0],hp_out[1],hp_out[2]), (10, 20), cv2.FONT_HERSHEY_COMPLEX, 0.25, (0, 255, 0), 1)
+            
+            if 'ge' in Flags:
+                x, y, w = int(gaze_vector[0]*12), int(gaze_vector[1]*12), 160
+                le =cv2.line(l_eye.copy(), (x-w, y-w), (x+w, y+w), (255,0,255), 2)
+                cv2.line(le, (x-w, y+w), (x+w, y-w), (255,0,255), 2)
+                re = cv2.line(r_eye.copy(), (x-w, y-w), (x+w, y+w), (255,0,255), 2)
+                cv2.line(re, (x-w, y+w), (x+w, y-w), (255,0,255), 2)
+                croppedFace[eye_coords[0][1]:eye_coords[0][3],eye_coords[0][0]:eye_coords[0][2]] = le
+                croppedFace[eye_coords[1][1]:eye_coords[1][3],eye_coords[1][0]:eye_coords[1][2]] = re
+                
+            cv2.imshow("visualization",cv2.resize(new_frame,(500,500)))
+
+
 
 
     
